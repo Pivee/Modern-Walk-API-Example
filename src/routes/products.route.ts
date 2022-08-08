a46d2eb5f905d6f209375d32fffca4c3e5faef50
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-08-08 17:10:41 +0530

    Integrate ProductsService

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express";
+import { ProductsService } from "../services/products.service";
+import products from "../mocks/products.json";
 
 const productsRouter = express.Router();
 
+const productsService = new ProductsService();
+
 productsRouter.get("/", (req: Request, res: Response) => {
-  res.send("Find all products");
+  res.json(productsService.getAllProducts(products));
+});
+
+productsRouter.get("/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  res.json(productsService.getProductById(products, id));
 });
 
 export default productsRouter;
