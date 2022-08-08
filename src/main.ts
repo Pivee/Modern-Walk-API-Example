de7d69213381eb2caf34b4d00a60f45beb3d2983
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-08-08 13:26:02 +0530

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
-import express, { Request, Response } from "express";
-import categoriesRouter from "./routes/categories.route";
-import productsRouter from "./routes/products.route";
-import tenantsRouter from "./routes/tenants.route";
-import usersRouter from "./routes/users.router";
+import app from "./app";
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
-
-app.use("/categories", categoriesRouter);
-app.use("/products", productsRouter);
-app.use("/tenants", tenantsRouter);
-app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
