9c304ad7699f33f6d37d1bd6c3e0b15f01e3001f
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-08-08 16:15:45 +0530

    Create Helper fn(): find

---
+++ src/helpers/find.helper.ts
@@ -0,0 +1,37 @@
+interface IModel {
+  [key: string]: any;
+}
+
+interface IFindOptions<T extends IModel, K> {
+  dataset: T[];
+  key?: string;
+  value?: K;
+}
+
+export class InvalidDatasetError extends Error {
+  message = "Dataset cannot be undefined";
+
+  constructor() {
+    super();
+    super.message = this.message;
+  }
+}
+
+export class InvalidKeyValuePair extends Error {
+  message = "Key of specified value is undefined";
+
+  constructor() {
+    super();
+    super.message = this.message;
+  }
+}
+
+export default function find<T extends IModel, K>(options: IFindOptions<T, K>) {
+  const { dataset, key, value } = options;
+
+  if (!dataset) throw new InvalidDatasetError();
+  if (!value) return dataset;
+
+  if (!key && value) throw new InvalidKeyValuePair();
+  if (key && value) return dataset.filter((datum: T) => datum[key] === value);
+}
