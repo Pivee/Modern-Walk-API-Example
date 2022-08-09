ec8401d485043ff1cb0e45e4c356969cf53ca5b8
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-08-09 15:46:22 +0530

    Integrate Swagger (using `tsoa`) for API Docs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import categoriesRouter from "./routes/categories.route";
 import productsRouter from "./routes/products.route";
 import tenantsRouter from "./routes/tenants.route";
 import usersRouter from "./routes/users.router";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -14,5 +15,17 @@ app.use("/api/v1/categories", categoriesRouter);
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/tenants", tenantsRouter);
 app.use("/api/v1/users", usersRouter);
+
+app.use(express.static("dist"));
+app.use(
+  "/api/swagger",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
+
 
 export default app;
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,7 +1,9 @@
+import { Get, Route } from "tsoa";
 import { IProduct } from "../interfaces/IProduct";
 import products from "../mocks/products.json";
 import { ProductsService } from "../services/products.service";
 
+@Route("api/v1/products")
 export default class ProductsController {
   productsDatasource;
   productsService;
@@ -11,10 +13,12 @@ export default class ProductsController {
     this.productsService = new ProductsService(this.productsDatasource);
   }
 
+  @Get("/")
   public getAllProducts(): IProduct[] {
     return this.productsService.getAllProducts();
   }
 
+  @Get("/:id")
   public getProductById(id: string): IProduct[] {
     return this.productsService.getProductById(id);
   }
